9f1f7c3dab392a0a31f04f5ab227688c2ee528ef
Azizul Hakimi Mohd Yussuf Izzudin <ahmyi@users.noreply.github.com>
2018-09-16 11:07:56 +0800

    Update for stellite config

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,34 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
-	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://www.masariwallet.com/api/',
-	mainnetExplorerUrl: "https://msrchain.net/",
-	testnetExplorerUrl: "http://testnet.msrchain.net/",
-	testnet: false,
-	coinUnitPlaces: 12,
+	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://wwtn.xtlpool.com/api/',
+
+	
+	coinUnitPlaces: 2,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	addressPrefix: 28,
-	integratedAddressPrefix: 29,
+
+	mainnetExplorerUrl: "https://explorer.stellite.cash/",
+	addressPrefix: 9241,
+	integratedAddressPrefix: 28822,
+	subAddressPrefix: 52,
+	
+	testnet: false,
+	testnetExplorerUrl: "http://testnet.msrchain.net/",
 	addressPrefixTestnet: 33,
 	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
 	subAddressPrefixTestnet: 73,
-	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
-	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	
+	feePerKB: new JSBigInt('15'),//20^10 - for testnet its not used, as fee is dynamic.
+	dustThreshold: new JSBigInt('1'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 12, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'MSR',
-	openAliasPrefix: "msr",
-	coinName: 'Masari',
-	coinUriPrefix: 'masari:',
-	avgBlockTime: 120,
+	coinSymbol: 'XTTL',
+	openAliasPrefix: "xtl",
+	coinName: 'Stellite',
+	coinUriPrefix: 'stellitecoin:',
+	avgBlockTime: 60,
 	maxBlockNumber: 500000000,
 };
